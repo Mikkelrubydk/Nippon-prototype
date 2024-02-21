d9954b241b0c80081b230aba36d5f9fe46ffdc90
MikkelRuby <mikkelruby1@hotmail.com>
2024-02-21 11:00:32 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+document.addEventListener('scroll', () => {
+    let nav = document.querySelector(`nav`);
+
+    if(window.scrollY > 0) {
+        nav.classList.add(`scrolled`)
+    }
+
+    else{
+        nav.classList.remove('scrolled')
+    }
+})
